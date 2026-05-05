3c91bca5705ba0a8334aa69a2a2b343ff9495851
Hamza-fin <hamzaahmedzahoor@gamil.com>
2026-05-05 11:10:08 +0500

    Add File

---
+++ list-indexing.py
@@ -0,0 +1,11 @@
+#Lists are ordered, indexed  and changeable
+
+my_list = ["House 123","Lane 5","Peshawar Road","Rawalpindi",54600]
+
+print(my_list,type(my_list),len(my_list))
+
+for items in my_list:
+  print(items)
+
+for i in range(len(my_list)):
+  print(my_list[i])
